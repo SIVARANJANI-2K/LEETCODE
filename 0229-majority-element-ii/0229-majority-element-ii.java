e263139956a61333b9ec68aaf69931f7374b0bf6
SIVARANJANI S <128780969+SIVARANJANI-2K@users.noreply.github.com>
2024-07-17 20:04:05 +0530

    Time: 2 ms (99.17%), Space: 48 MB (45.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
         int n=nums.length;
+        int majority1=0;
+        int majority2=0;
+        int count1=0;
+        int count2=0;
         for(int i=0;i<n;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
+            if(majority1==nums[i]){
+                count1+=1;
+            }
+            else if(majority2==nums[i]){
+                count2+=1;
+            }
+            else if(count1==0){
+                majority1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                majority2=nums[i];
+                count2=1;
             }
             else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-            if(map.get(nums[i])>Math.floor(n/3) && !res.contains(nums[i])){
-                    res.add(nums[i]);
+                count1-=1;
+                count2-=1;
             }
         }
+        count1=0;
+        count2=0;
+        for(int num:nums){
+            if(num==majority1)
+                count1++;
+            else if(num==majority2)
+                count2++;
+        }
+        if(count1>Math.floor(n/3))
+            res.add(majority1);
+        if(count2>Math.floor(n/3))
+            res.add(majority2);
         return res;
     }
 }
