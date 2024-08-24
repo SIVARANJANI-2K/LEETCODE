83a143cff60d0c4a7f50e3b56c7f5114bd294b11
SIVARANJANI S <128780969+SIVARANJANI-2K@users.noreply.github.com>
2024-08-24 16:16:04 +0530

    Time: 3 ms (24.25%), Space: 45.9 MB (47.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int zeros=0;
         int first=0;
-        int second=first+1;
         while(first<nums.length){
           if(nums[first]==0){
-              while(second<nums.length && nums[second]==0){
-                  second++;
-              }
-              if(second<nums.length){
-                   nums[first]=nums[second];
-                   nums[second]=0;
-              }
-             
-              
+              zeros++;  
           }
+          else{ 
+                  int swap=nums[first-zeros];
+                  nums[first-zeros]=nums[first];
+                  nums[first]=swap;
+              
+           }
           first++;
-          second++;
+        
         }
     }
 }
